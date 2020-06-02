3ee4501fa2565942de5d2632b656ea8ce25e606f
Brandt Campbell <BCampbell@talentpath.com>
2020-06-02 14:45:56 -0400

    added dependencies

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
-    
+
     @Override
     public List<Speaker> findAll() {
         return repository.findAll();
